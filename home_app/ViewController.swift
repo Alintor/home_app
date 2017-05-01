875300617c62e57cc275ae799cd4990dc2a37980
Alexandr Ovchinnikov <Ovchinnikov.Alex@softomate.com>
2017-05-01 20:45:21 +0700

    add Xcode project

---
+++ home_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  home_app
+//
+//  Created by Alexandr Ovchinnikov on 01/05/2017.
+//  Copyright © 2017 Ovchinnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
